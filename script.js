32e7ea7150105d5de962dc3a198ab24938a63bdd
Alex Radoi <123100117+DonComedia1408@users.noreply.github.com>
2025-01-13 09:34:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let nombre = "Alejandro Cristian Radoi";
+let edad = 18;
+let ciudad = "Madrid";
+let tipoDeEstudio = "Formación profesional";
+
+console.log("Perfil de usuario actual:");
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("Ciudad:", ciudad);
+console.log("Tipo de estudio:", tipoDeEstudio);
+
+ciudad = "Nuevo Baztan";
+
+console.log("Perfil de usuario actualizado:");
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("Ciudad:", ciudad);
+console.log("Tipo de estudio:", tipoDeEstudio);
